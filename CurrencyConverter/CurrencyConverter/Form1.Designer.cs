4619d5a9c9ff6b42f19ca6c2e655ab476d028010
Shofiyah Mardhiyah <54921573+Shofi52@users.noreply.github.com>
2021-03-31 03:47:11 +0700

    Add files via upload

---
+++ CurrencyConverter/CurrencyConverter/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace CurrencyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.input = new System.Windows.Forms.NumericUpDown();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.input)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "AUD",
+            "CNY",
+            "EUR",
+            "IDR",
+            "KRW",
+            "MYR",
+            "PHP",
+            "USD"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 88);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = " ";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(303, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Currency Converter";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "AUD",
+            "CNY",
+            "EUR",
+            "IDR",
+            "KRW",
+            "MYR",
+            "PHP",
+            "USD"});
+            this.comboBox2.Location = new System.Drawing.Point(194, 88);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 23);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = " ";
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "From:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(194, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To:";
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(12, 140);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(121, 23);
+            this.input.TabIndex = 5;
+         //   this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            this.input.ValueChanged += new System.EventHandler(this.input_ValueChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(194, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(121, 23);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Enter Amount:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 185);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.input)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown input;
+    }
+}
+
---
+++ CurrencyConverter/CurrencyConverter/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyConverter
+{
+    public partial class Form1 : Form
+    {
+        private List<Currency> currencies;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCurrency();
+            SetUpComponent();
+            Ganti();
+        }
+
+        private void CreateNewCurrency(string name, float rate)
+        {
+            currencies.Add(new Currency(name, rate));
+        }
+        private void InitializeCurrency()
+        {
+            // https://www.xe.com/
+            currencies = new List<Currency>();
+            CreateNewCurrency("IDR", 1.0f);
+            CreateNewCurrency("AUD", 0.000090837f);
+            CreateNewCurrency("CNY", 0.00045323f);
+            CreateNewCurrency("EUR", 0.000058798f);
+            CreateNewCurrency("KRW", 0.078216f);
+            CreateNewCurrency("MYR", 0.00028620f);
+            CreateNewCurrency("PHP", 0.0033467f);
+            CreateNewCurrency("USD", 0.000068961f);  
+        }
+
+        private void SetUpComponent()
+        {
+            foreach (Currency currency in currencies)
+            {
+                comboBox1.Items.Add(currency.currencyCode);
+                comboBox2.Items.Add(currency.currencyCode);
+            }
+
+            input.Minimum = 0;
+            input.Maximum = decimal.MaxValue;
+            input.DecimalPlaces = 2;
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+
+            input.ValueChanged += input_ValueChanged;
+        }
+
+        private Currency GetCurrencyByName(string name)
+        {
+            for (int i = 0; i < currencies.Count; i++)
+            {
+                if (currencies[i].currencyCode == name)
+                {
+                    return currencies[i];
+                }
+            }
+            return null;
+        }
+
+        private void Ganti()
+        {
+            float money = (float)input.Value;
+            Currency inCurrency = GetCurrencyByName(comboBox1.Text);
+            Currency outCurrency = GetCurrencyByName(comboBox2.Text);
+           
+            if (outCurrency != null)
+           {
+                float output = inCurrency.ConvertTo(money, outCurrency);
+                string hasil = output.ToString();
+                textBox2.Text = hasil;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Ganti();
+        }
+
+        private void input_TextChanged(object sender, EventArgs e)
+        {
+            Ganti();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Ganti();
+        }
+
+        private void input_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+    class Currency
+    {
+        public string currencyCode { get; }
+        private float rate;
+
+        public Currency(string name, float rate)
+        {
+            this.currencyCode = name;
+            this.rate = rate;
+        }
+
+        public float ConvertTo(float money, Currency currency)
+        {
+            return money / rate * currency.rate;
+        }
+    }
+
+}
